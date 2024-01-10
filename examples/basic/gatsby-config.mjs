6597719f76ab1e3392871ab08f36d08fa8acaa98
Dan Greaves <dan@dangreaves.com>
2024-01-10 17:16:13 +1100

    Enable asset prefix in examples

--- examples/basic/gatsby-config.mjs
+++ examples/basic/gatsby-config.mjs
@@ -3,6 +3,7 @@ import { createAdapter } from "@dangreaves/gatsby-adapter-aws/adapter.js";
 /** @type {import('gatsby').GatsbyConfig} */
 export default {
   adapter: createAdapter(),
+  assetPrefix: "/assets",
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
