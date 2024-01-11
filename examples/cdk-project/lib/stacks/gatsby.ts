4a5ec89b7351d5d7e3605886189dacd16f8171ff
Dan Greaves <dan@dangreaves.com>
2024-01-11 14:42:21 +1100

    Deploy SSR example to fargate in cdk-project example

--- examples/cdk-project/lib/stacks/gatsby.ts
+++ examples/cdk-project/lib/stacks/gatsby.ts
@@ -1,7 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-// import { GatsbySite } from "@dangreaves/gatsby-adapter-aws/cdk.js";
+import { GatsbySite } from "@dangreaves/gatsby-adapter-aws/cdk.js";
 
 export interface GatsbyStackProps extends cdk.StackProps {
   vpc: ec2.IVpc;
@@ -11,6 +11,12 @@ export class GatsbyStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, { vpc, ...props }: GatsbyStackProps) {
     super(scope, id, props);
 
-    // new GatsbySite(this, "GatsbySite", { vpc });
+    new GatsbySite(this, "GatsbySite", {
+      vpc,
+      gatsbyDir: "../../examples/ssr",
+      ssrExecutorOptions: {
+        target: "FARGATE",
+      },
+    });
   }
 }
